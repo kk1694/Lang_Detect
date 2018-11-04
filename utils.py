e5af6595ca9b61b83471cc5984144c56dc63384b
Krisztian Kovacs <krisztiankovacs@fastmail.com>
2018-11-04 09:42:01 +0000

    Add language classification model.

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import re
 from pathlib import Path
 import numpy as np
 import torch
+from tqdm import tqdm
 
 def preprocess(txt):
     
@@ -39,22 +40,27 @@ def preprocess(txt):
 def concat_docs(lang, train_dir):
     '''Concatenate all texts for a language.'''
     res = ''
+    errors = 0
     fns = list((train_dir/lang).glob('*.txt'))
     for fn in fns:
-        res = res + fn.read_text()
+        try:
+            res = res + fn.read_text()
+        except:
+            errors += 1
+    if errors > 0:
+        print(f'\n{errors} files not loaded for {lang}')
     return res
 
-def txt2list(txt, min_chars = 10):
+def txt2list(txt, min_chars = 3):
     
-    '''Preprocess text, and convert to list of sentences.'''
+    '''Preprocess text, and converts it to a list of sentences.'''
     
     res = preprocess(txt)
     res = res.strip().split('<eos>')
-    
-    # We also want to remove items consting only of whitespace and numbers
     res = list(map(lambda x: x.strip(), res))
     
-    # Note for the minimum size: ' <eos> ' is already 7 long
+    # We also want to remove items consting only of whitespace,
+    # or only of numbers, or less than min_char characters.   
     res = list(filter(lambda x: x != '' and x != '<num>' and len(x) >= min_chars, res))
     res = list(map(lambda x: x + ' <eos>', res))
     
@@ -73,16 +79,19 @@ def concat_random_sent(txt, p = 0.02):
     
     return res
 
-def numericalize(X, word2idx, maxlen = 50):
+def numericalize(X, word2idx, maxlen = 32, pad = '<pad>'):
+    '''Converts a (m,) array of sentences into a (m, maxlen) array of word indices.'''    
     m = X.shape[0]
-    res = np.ones((m, maxlen))
-    for row in range(m):
+    pad_idx = word2idx[pad]
+    res = np.ones((m, maxlen)) * pad_idx  # Empty array of padding
+    for row in tqdm(range(m), leave = False):
         temp = np.array([word2idx[w] for w in X[row].split()])
-        n = min(len(temp), maxlen)
+        n = min(len(temp), maxlen)  # Truncate sentence at maxlen words.
         res[row, :n] = temp[:n]
     return res.astype(np.int32)
 
 def de_numericalize(X, idx2word):
+    '''Converts an array of word indices into a list of sentences.'''
     res = []
     for row in range(X.shape[0]):
         res.append(' '.join([idx2word[i] for i in X[row][X[row] != 1]]))
@@ -146,3 +155,17 @@ def skipgram_data(X, idx_freq, k = 5, as_tensor = True):
         y = torch.from_numpy(y).type(torch.float32)
 
     return (X_res, y)
+
+def exp_smooth(x, beta, bias_correct = True):
+    if beta == 0:
+        return x
+    m = x.shape[0]
+    res = np.zeros(m)
+    res[0] = (1 - beta)*x[0]
+    for i in range(m):
+        res[i] = beta*res[i-1] + (1 - beta)*x[i]
+    if bias_correct:
+        corr = 1 - beta ** (np.arange(m) + 1)
+        assert corr.shape == res.shape
+        res = res / corr
+    return res
