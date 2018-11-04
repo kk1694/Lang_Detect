de2564eac82eff1567e4134a9b5f89b568dd8c8c
Krisztian Kovacs <krisztiankovacs@fastmail.com>
2018-11-04 10:15:48 +0000

    Remove create embedding section (doesn't seem to be necessary).

--- utils.py
+++ utils.py
@@ -97,65 +97,6 @@ def de_numericalize(X, idx2word):
         res.append(' '.join([idx2word[i] for i in X[row][X[row] != 1]]))
     return res
 
-def subsamp_disc_prob(idx_freq):
-    '''Creates an index of discard probabilities for the skip-gram subsampling.'''
-    const = 1e-5  # From Mikolov et al 2013
-    rel_freq = idx_freq / (np.sum(idx_freq) - idx_freq[1])
-    rel_freq[1] = 1  # We treat padding separately, otherwise it skews freq distribution
-    disc_prob = 1 - np.sqrt(np.minimum(const / rel_freq, 1))
-    return disc_prob
-
-def negsamp_prob(idx_freq):
-    const = 3/4  # From Mikolov et al 2013
-    rel_freq = idx_freq / (np.sum(idx_freq))
-    rel_freq[1] = 0.01  # Manually assign 1% prob to padding
-    rel_freq = rel_freq ** const
-    rel_freq = rel_freq / np.sum(rel_freq)
-    return rel_freq
-
-def skipgram_data(X, idx_freq, k = 5, as_tensor = True):
-    
-    m, n = X.shape
-
-    # Create correct context-target pairs
-    context_idx = np.random.randint(0, n, size = m)
-    target_idx = np.random.randint(1, k, size = m)
-    target_idx = (np.random.randint(0, 2, size = m)*2-1)*target_idx + context_idx
-    target_idx = np.minimum(np.maximum(target_idx, 0), n-1)
-    context = X[range(m), context_idx]
-    target = X[range(m), target_idx]
-    
-    # Discard frequent words
-    disc_prob = subsamp_disc_prob(idx_freq)
-    keep_mask = (np.random.rand(m) > disc_prob[context])
-    context = context[keep_mask]
-    target = target[keep_mask]
-    
-    correct_num, fake_num, total  = (len(context), len(context)*5, len(context)*6)
-
-    # Create incorrect pairs
-    neg_samp_p = negsamp_prob(idx_freq)
-    fake_target = np.random.choice(len(idx_freq), fake_num, p=neg_samp_p)
-    fake_context = np.repeat(context, k)
-    
-    # Concatenate all
-    context = np.concatenate((context, fake_context)).reshape((total, 1))
-    target = np.concatenate((target, fake_target)).reshape((total, 1))
-    X_res = np.concatenate((context, target), axis = 1)
-    
-    # Classification target
-    y = np.concatenate((np.ones(correct_num), np.zeros(fake_num)))
-
-    # Shuffle data
-    shufl = np.random.permutation(total)
-    X_res, y = (X_res[shufl], y[shufl])
-    
-    if as_tensor:   # Convert to pytorch tensor
-        X_res = torch.from_numpy(X_res).type(torch.int64)
-        y = torch.from_numpy(y).type(torch.float32)
-
-    return (X_res, y)
-
 def exp_smooth(x, beta, bias_correct = True):
     if beta == 0:
         return x
